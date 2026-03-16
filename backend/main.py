b7ad6a1ca1f71dcde8f69ed65eca50990924625e
Vishwas Charan <vishwascharan11@gmail.com>
2026-03-16 17:18:36 +0530

    updated entry point

--- backend/main.py
+++ backend/main.py
@@ -6,8 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://rag-pdf-analyzer.vercel.app/",
-                   "http://localhost:5173",],
+    allow_origins=["https://rag-pdf-analyzer.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
