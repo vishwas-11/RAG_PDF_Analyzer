6e8d0fd3ee33a9233056517ae9bce1eba4ea0603
Vishwas Charan <vishwascharan11@gmail.com>
2026-03-16 17:14:24 +0530

    added frontend origin

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://rag-pdf-analyzer.vercel.app/",
+                   "http://localhost:5173",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
